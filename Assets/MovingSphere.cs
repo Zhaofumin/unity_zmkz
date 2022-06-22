90bcb9b77f4b7b631e6402ccf10e6ada774fc9af
zfm <yxjzjtzfm12345@foxmail.com>
2022-06-22 21:26:55 +0800

    main_zfm_jump_ok_jumpnum_2022_6_22_21_26

--- Assets/MovingSphere.cs
+++ Assets/MovingSphere.cs
@@ -8,34 +8,56 @@ public class MovingSphere : MonoBehaviour
     private Rigidbody rd;
     public Transform main_form;
     public Transform playertransform;
+    public Transform jiao;
+    public LayerMask ground;
+    [SerializeField,Range(0f,300f)]
+    float speed;
+    [SerializeField, Range(0f, 100f)]
+    float js;
+    public int maxjump;
     private Vector3 offset;
+    int jump ;
+    private CharacterController _characterController;
     // Start is called before the first frame update
     void Start()
     {
+        jump = maxjump;
         //Debug.Log("game started");
         rd = GetComponent<Rigidbody>();
         offset = main_form.position - playertransform.position;
+        _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h=Input.GetAxis("Horizontal");
-        rd.AddForce(new Vector3(5*h, 0, 0));
-        transform.eulerAngles = new Vector3(0, 0, 0);
-        main_form.position = playertransform.position + offset;
-        if ((!Input.GetKeyDown("d"))&& (!Input.GetKeyDown("a")))
-        {
-            rd.AddForce(new Vector3(-rd.velocity.x, 0, 0));
-
-        }
-        if (Input.GetKeyDown("w")) 
+        bool isGround = Physics2D.OverlapCircle(jiao.position, 0.1f, ground);
+        if (Input.GetKeyDown(KeyCode.Space)&&jump>0)
         {
             rd.AddForce(new Vector3(0, 600, 0));
-
-        
+            jump--;
         }
-        if (rd.velocity.x>9)
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        main_form.position = playertransform.position + offset;
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        jump = maxjump;
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rd.AddForce(new Vector3(speed * h, 0, 0));
+
+        if (!(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            rd.AddForce(new Vector3(-rd.velocity.x * js, 0, 0));
+
+        }
+
+        if (rd.velocity.x > 9)
+
         {
             rd.AddForce(new Vector3(-5 * h, 0, 0));
 
@@ -44,7 +66,6 @@ public class MovingSphere : MonoBehaviour
         {
             rd.AddForce(new Vector3(-5 * h, 0, 0));
         }
-
         transform.position = new Vector3(transform.position.x, transform.position.y, 0);
     }
 }
